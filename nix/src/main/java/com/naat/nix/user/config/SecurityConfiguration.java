e67fd20a7af4d39fbc22b4a9989c07b70aef184c
equiroz <equiroz@ciencias.unam.mx>
2020-05-02 18:48:12 -0500

    Detalles de uso de usuarios con roles

--- nix/src/main/java/com/naat/nix/user/config/SecurityConfiguration.java
+++ nix/src/main/java/com/naat/nix/user/config/SecurityConfiguration.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
   @Autowired
