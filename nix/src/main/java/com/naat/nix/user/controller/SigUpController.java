a0410d5affadb8714e7ffe156b4bdfa4bc6e9d52
equiroz <equiroz@ciencias.unam.mx>
2020-05-04 20:35:30 -0500

    Quitando dependencias innecesarias

--- nix/src/main/java/com/naat/nix/user/controller/SigUpController.java
+++ nix/src/main/java/com/naat/nix/user/controller/SigUpController.java
@@ -1,17 +1,17 @@
 package com.naat.nix.user.controller;
 
-import com.naat.nix.user.controller.SecurityService;
-import com.naat.nix.user.controller.UserService;
-import com.naat.nix.validator.UserValidator;
-import com.naat.nix.user.config.UserWrapper;
-import com.naat.nix.user.model.User;
 import javax.validation.Valid;
-import org.springframework.stereotype.Controller;
+
+import com.naat.nix.user.model.User;
+import com.naat.nix.validator.UserValidator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SigUpController {
