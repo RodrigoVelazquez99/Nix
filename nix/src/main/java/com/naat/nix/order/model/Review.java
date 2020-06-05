ddee08c73947b4623b1c70eebbafb4eab76c7caf
equiroz <equiroz@ciencias.unam.mx>
2020-06-05 04:25:16 -0500

    Restoring review

---
+++ nix/src/main/java/com/naat/nix/order/model/Review.java
@@ -0,0 +1,11 @@
+package com.naat.nix.order.model;
+
+import lombok.Data;
+
+/**
+ * Representa información del formulario de calificación
+ */
+@Data
+public class Review {
+  private ScoreValue score;
+} 
